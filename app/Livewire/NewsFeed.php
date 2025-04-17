334205797f1b7e63915b528c40ef5746169ce62b
Oskar Kownacki <legromp123@gmail.com>
2025-04-17 19:06:02 +0200

    NewsFeed work

--- app/Livewire/NewsFeed.php
+++ app/Livewire/NewsFeed.php
@@ -8,12 +8,23 @@ use Livewire\Component;
 class NewsFeed extends Component
 {
     public $articles = [];
+    public $fullscreen = false;
+    public $selectedArticle = null;
 
     public function boot(){
         $apikey = $_ENV["NEWSAPI_KEY"];
         $response = Http::get('https://newsapi.org/v2/everything?q=bitcoin&language=en&apiKey='.$apikey);
         $this->articles = $response->object()->articles;
     
+    }
+
+    public function openFullscreen($index){
+        $this->selectedArticle = $this->articles[$index];
+        $this->fullscreen = true;
+    }
+    public function closeFullscreen(){
+        $this->selectedArticle = null;
+        $this->fullscreen = false;
     }
     public function render()
     {
--- resources/views/livewire/news-feed.blade.php
+++ resources/views/livewire/news-feed.blade.php
@@ -3,10 +3,31 @@
     @foreach($articles as $index => $article)
     <div class="col-md-4  mb-2">
         <div class="card h-100 border border-dark p-3 bg-dark text-white">
-        <h3>{{$article->title}}</h3>
+        <h3 role="button" wire:click="openFullscreen({{$index}})">{{$article->title}}</h3>
         <p class="mb-0">{{ $article->description }}</p>
         </div>
     </div>
     @endforeach
+
+    @if($selectedArticle)
+    <div class="modal fade show d-block" tabindex="-1" style="background-color: rgba(0,0,0,0.5)">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">{{$selectedArticle->title}}</h5>
+              <button type="button" class="btn-close" wire:click="closeFullscreen" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="{{ $selectedArticle->urlToImage }}" class="img-fluid rounded mb-4">
+              <p>{{$selectedArticle->description}}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" wire:click="closeFullscreen">Close</button>
+              <a class="btn btn-primary" href={{ $selectedArticle->url }}>Check out the article!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    @endif
     </div>
 </div>
