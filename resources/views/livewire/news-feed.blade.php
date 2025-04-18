12ce199f7da2f16e9036d5384fdaa0c07837c904
Oskar Kownacki <legromp123@gmail.com>
2025-04-18 14:52:27 +0200

    adding nice transition

--- resources/views/livewire/news-feed.blade.php
+++ resources/views/livewire/news-feed.blade.php
@@ -11,7 +11,7 @@
     @endforeach
 
     @if($selectedArticle)
-    <div class="modal fade show d-block" tabindex="-1" style="background-color: rgba(0,0,0,0.5)">
+    <div class="modal fade show d-block" tabindex="-1" style="background-color: rgba(0,0,0,0.5)"  wire:transition>
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
